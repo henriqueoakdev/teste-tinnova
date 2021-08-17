b35112fad8427dcab4303fc627b4bbe8deae31a0
Henrique Carvalho <henrique.dev.contato@gmail.com>
2021-08-17 09:36:09 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/henriqueoak/testetinnova/TesteTinnova5Application.java
@@ -0,0 +1,13 @@
+package com.henriqueoak.testetinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTinnova5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTinnova5Application.class, args);
+	}
+
+}
