2e8a11f3553e15f3db1a77334a3a9627d918467a
Henrique Carvalho <henrique.dev.contato@gmail.com>
2021-08-17 15:34:43 -0300

    Obter usuário por ID

--- src/main/java/com/henriqueoak/testetinnova/services/VeiculoService.java
+++ src/main/java/com/henriqueoak/testetinnova/services/VeiculoService.java
@@ -1,12 +1,14 @@
 package com.henriqueoak.testetinnova.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henriqueoak.testetinnova.domain.Veiculo;
 import com.henriqueoak.testetinnova.repository.VeiculoRepository;
+import com.henriqueoak.testetinnova.services.exception.ObjectNotFoundException;
 
 @Service
 public class VeiculoService {
@@ -17,4 +19,9 @@ public class VeiculoService {
 	public List<Veiculo> findAll(){
 		return v.findAll();
 	}
+	
+	public Veiculo findById(String id) {
+		Optional<Veiculo> obj = v.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
